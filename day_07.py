ca2ca8337ef020ad277781d4548340fcb3b70a0a
jasonbrackman <brackman@gmail.com>
2021-12-06 21:53:23 -0800

    added typehints

--- day_07.py
+++ day_07.py
@@ -1,21 +1,22 @@
 import sys
+from typing import Callable, List
 
 import helpers
 
 
-def get_cost(n):
+def get_cost(n: int) -> float:
     return n * (n + 1) / 2
 
 
-def run():
+def run() -> None:
     lines = helpers.get_lines(r"./data/day_07.txt")
     nums = sorted([int(i) for i in lines[0].split(",")])
 
-    assert fuel_cost(nums) == 355521
+    assert fuel_cost(nums, cost_func=lambda x: x) == 355521
     assert fuel_cost(nums, cost_func=get_cost) == 100148777
 
 
-def fuel_cost(nums, cost_func=lambda x: x):
+def fuel_cost(nums: List[int], cost_func: Callable) -> int:
     total = sys.maxsize
     for index in range(nums[0], nums[-1] + 1):
         temp = sum(
